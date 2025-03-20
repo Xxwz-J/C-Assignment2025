5066e2c8d2ae96327d10e51170688cb376ceb8dc
Xxwz-J <2023302111332.whu.edu.cn>
2025-03-20 10:52:48 +0800

    assignment4

---
+++ assignment4/homework4_1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Security.AccessControl;
+using static homework4_1.Program;
+
+namespace homework4_1
+{
+    internal class Program
+    {
+       
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; } 
+            public T Data { get; set; }        
+
+            
+            public Node(T t)
+            {
+                Next = null;  
+                Data = t;      
+            }
+        }
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get => head;
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);  
+                if (tail == null)
+                {        
+                    head = tail = n;
+                }
+                else
+                {                    
+                    tail.Next = n;            
+                    tail = n;                
+                }
+            }
+
+            public void ForEach(Action<Node<T>> action)
+            {
+                Node < T > p = head;
+                while(p != null) {
+                    action(p);
+                    p= p.Next;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+           GenericList<int> list = new GenericList<int>();
+            list.Add(2);
+            list.Add(1);
+            list.Add(4);
+            list.Add(3);
+            int min = int.MaxValue, max = int.MinValue;
+            double sum=0, lenth=0;
+            list.ForEach(item => { 
+            if(item.Data >max) max= item.Data;
+            if(item.Data <min) min= item.Data;
+            lenth++;
+            sum+= item.Data;
+            });
+            Console.WriteLine($"max:{max} min:{min} sum:{sum} ave:{(double)sum / (double)lenth}");
+
+        }
+    }
+}
---
+++ assignment4/homework4_2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+using System.Timers;
+namespace homework4_2
+{
+    internal class Program
+    {
+        static System.Timers.Timer timer = new System.Timers.Timer(1000);
+        public delegate void MyEventHandler(object sender, Time e);
+        public struct Time
+        {
+            public int hour, minute, second;
+            public Time(int h,int m,int s) { hour = h; minute=m; second=s;}
+            public void  Inc() {
+                second++;
+                if(second == 60) {
+                    minute++;
+                    second = 0;
+                }
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                }
+                if (hour == 24) hour = 0;
+            }
+        }
+        public class Clock {
+
+            Time now, alarmTime;
+
+            public Clock() { 
+                now=alarmTime= new Time(0,0,0);
+            }
+            public void Set(int h,int m,int s) { now = new Time(h, m, s); }
+
+            public void SetAlarm(int h, int m, int s) { alarmTime = new Time(h, m, s); }
+            public event MyEventHandler OnTick;
+            public event MyEventHandler OnAlarm;
+            public void Tick(object sender, ElapsedEventArgs e)
+            {
+                
+                OnTick?.Invoke(this, now);
+                now.Inc();
+                
+                if (now.hour == alarmTime.hour &&
+                    now.minute == alarmTime.minute &&
+                    now.second == alarmTime.second)
+                {
+                    OnAlarm?.Invoke(this, now);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            timer.AutoReset = true;
+            timer.Elapsed += clock.Tick;
+            clock.OnTick += new MyEventHandler(Clock_Tick);
+            clock.OnAlarm += new MyEventHandler(Clock_Alarm);
+            void Clock_Tick(object sender, Time e)
+            {
+                Console.WriteLine($"Tick!{e.hour}:{e.minute}:{e.second}");
+            }
+            void Clock_Alarm(object sender, Time e)
+            {
+                Console.WriteLine($"Alarm!{e.hour}:{e.minute}:{e.second}");
+            }
+            clock.SetAlarm(0, 0, 5);
+            timer.Start(); 
+            Console.ReadLine(); 
+        }
+    }
+}
