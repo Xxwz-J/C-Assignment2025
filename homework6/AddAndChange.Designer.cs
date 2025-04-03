b73de2fe97ff5b60e63c9a8b8c5247e61a12e3fa
Xxwz-J <2023302111332.whu.edu.cn>
2025-04-03 17:41:37 +0800

    homework6 not completed

---
+++ homework6/AddAndChange.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace homework6
+{
+    partial class AddAndChange
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.labelID = new System.Windows.Forms.Label();
+            this.labelCustomerID = new System.Windows.Forms.Label();
+            this.textBoxCustomerID = new System.Windows.Forms.TextBox();
+            this.labelCustomerName = new System.Windows.Forms.Label();
+            this.textBoxCustomerName = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonChange = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxID.Location = new System.Drawing.Point(738, 68);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(145, 25);
+            this.textBoxID.TabIndex = 0;
+            // 
+            // labelID
+            // 
+            this.labelID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelID.AutoSize = true;
+            this.labelID.Location = new System.Drawing.Point(258, 73);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(23, 15);
+            this.labelID.TabIndex = 1;
+            this.labelID.Text = "ID";
+            // 
+            // labelCustomerID
+            // 
+            this.labelCustomerID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelCustomerID.AutoSize = true;
+            this.labelCustomerID.Location = new System.Drawing.Point(226, 234);
+            this.labelCustomerID.Name = "labelCustomerID";
+            this.labelCustomerID.Size = new System.Drawing.Size(87, 15);
+            this.labelCustomerID.TabIndex = 3;
+            this.labelCustomerID.Text = "CustomerID";
+            // 
+            // textBoxCustomerID
+            // 
+            this.textBoxCustomerID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxCustomerID.Location = new System.Drawing.Point(741, 229);
+            this.textBoxCustomerID.Name = "textBoxCustomerID";
+            this.textBoxCustomerID.Size = new System.Drawing.Size(138, 25);
+            this.textBoxCustomerID.TabIndex = 2;
+            // 
+            // labelCustomerName
+            // 
+            this.labelCustomerName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelCustomerName.AutoSize = true;
+            this.labelCustomerName.Location = new System.Drawing.Point(218, 396);
+            this.labelCustomerName.Name = "labelCustomerName";
+            this.labelCustomerName.Size = new System.Drawing.Size(103, 15);
+            this.labelCustomerName.TabIndex = 5;
+            this.labelCustomerName.Text = "CustomerName";
+            // 
+            // textBoxCustomerName
+            // 
+            this.textBoxCustomerName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxCustomerName.Location = new System.Drawing.Point(740, 391);
+            this.textBoxCustomerName.Name = "textBoxCustomerName";
+            this.textBoxCustomerName.Size = new System.Drawing.Size(140, 25);
+            this.textBoxCustomerName.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.labelID, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxCustomerName, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelCustomerName, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelCustomerID, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxCustomerID, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxID, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1081, 486);
+            this.tableLayoutPanel1.TabIndex = 6;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1087, 616);
+            this.tableLayoutPanel2.TabIndex = 8;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonAdd.Location = new System.Drawing.Point(91, 10);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(113, 79);
+            this.buttonAdd.TabIndex = 8;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.buttonChange, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.buttonAdd, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(247, 504);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(593, 100);
+            this.tableLayoutPanel3.TabIndex = 9;
+            // 
+            // buttonChange
+            // 
+            this.buttonChange.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonChange.Location = new System.Drawing.Point(386, 10);
+            this.buttonChange.Name = "buttonChange";
+            this.buttonChange.Size = new System.Drawing.Size(116, 79);
+            this.buttonChange.TabIndex = 9;
+            this.buttonChange.Text = "Change";
+            this.buttonChange.UseVisualStyleBackColor = true;
+            // 
+            // AddAndChange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1087, 616);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Name = "AddAndChange";
+            this.Text = "AddAndChange";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label labelID;
+        private System.Windows.Forms.Label labelCustomerID;
+        private System.Windows.Forms.TextBox textBoxCustomerID;
+        private System.Windows.Forms.Label labelCustomerName;
+        private System.Windows.Forms.TextBox textBoxCustomerName;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button buttonChange;
+    }
+}
---
+++ homework6/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using homework5;
+namespace homework6
+{
+    public partial class Form1 : Form
+    {
+        static OrderService orderService = new OrderService();
+        public Form1()
+        {
+            InitializeComponent();
+            
+            Customer c1 = new Customer("雷军", "001");
+            List<OrderDetails> Details = new List<OrderDetails> { new OrderDetails(new Product("xiaomiSU7","001",200000),1)};
+            orderService.AddOrder("001", c1, Details);
+            Service.DataSource = orderService.orders;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            Service.DataSource = orderService.orders.Where(s=>
+            s.Customer.Name == textBox1.Text || textBox1.Text=="");
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            new AddAndChange(new Order(),ref orderService).ShowDialog();
+        }
+    }
+
+
+}
